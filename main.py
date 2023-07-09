ad58220b9792238d50a4d0f0ba78acf282738744
Pavel Syu <poet.poet@gmail.com>
2023-07-09 20:46:17 +0300

    added plot, also removed erroneous new lines at the end of data.csv.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
 def analyze_data(filename):
@@ -38,6 +39,35 @@ def output_max_hour_operations(filename, max_hour):
     return operations
 
 
+def analyze_data2(filename):
+    data = defaultdict(int)
+
+    with open(filename, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip the header row
+
+        for row in csv_reader:
+            date = row[0]
+            operation_count = int(row[2])
+
+            data[date] += operation_count
+
+    dates = list(data.keys())
+    operation_counts = list(data.values())
+
+    return dates, operation_counts
+
+
+def plot_operations_over_time(dates, operation_counts):
+    plt.plot(dates, operation_counts)
+    plt.xlabel('Date')
+    plt.ylabel('Sum of Count of Operations')
+    plt.title('Sum of Count of Operations Over Time')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
 filename = 'data.csv'  # Replace 'data.csv' with the actual filename
 
 max_hour, max_count = analyze_data(filename)
@@ -48,3 +78,8 @@ operations = output_max_hour_operations(filename, max_hour)
 print(f"\nOperations and their intensities within the maximal hour ({max_hour}):")
 for operation, intensity in operations.items():
     print(f"Operation: {operation}\tIntensity: {intensity}")
+
+
+# make a plot
+dates, operation_counts = analyze_data2(filename)
+plot_operations_over_time(dates, operation_counts)
